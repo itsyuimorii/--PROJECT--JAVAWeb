1b7283907edf4ab0b2eff156dde923dec8ab9e95
YUIMORII <yuimoriistudio@gmail.com>
2022-12-07 15:05:48 -0600

    Servlet review

--- _06_Servlet/src/com/yuimorii/servlet/ContextServlet.java
+++ _06_Servlet/src/com/yuimorii/servlet/ContextServlet.java
@@ -9,7 +9,6 @@ import java.io.IOException;
 
 public class ContextServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
