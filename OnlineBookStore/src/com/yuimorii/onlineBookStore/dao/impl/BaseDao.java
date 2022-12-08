c54cb2348aaa3e58c87910b7b80ec3fed7319938
YUIMORII <yuimoriistudio@gmail.com>
2022-12-08 12:29:13 -0600

    update

---
+++ OnlineBookStore/src/com/yuimorii/onlineBookStore/dao/impl/BaseDao.java
@@ -0,0 +1,103 @@
+package com.yuimorii.onlineBookStore.dao.impl;
+
+import com.atguigu.utils.JdbcUtils;
+import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.handlers.BeanHandler;
+import org.apache.commons.dbutils.handlers.BeanListHandler;
+import org.apache.commons.dbutils.handlers.ScalarHandler;
+
+import java.sql.Connection;
+
+
+//為了重複使用代碼， 不需要對象實例
+public abstract class BaseDao {
+    //使用DBUtils操作數據庫
+    private QueryRunner queryRunner = new QueryRunner();
+
+    /**
+     * update() 方法用來執行， Insert\update\Delete語句
+     *
+     * @return() 如果返回-1， 說明執行失敗，返回其他表示影響的行數
+     */
+    public int update(String sql, Object... args) {
+        Connection connection = JdbcUtils.getConnection();
+        try {
+            queryRunner.update(connection, sql, args);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            JdbcUtils.close(connection);
+        }
+        return -1;
+    }
+
+
+
+
+    /**
+     * 查询返回一个javaBean的sql语句
+     *
+     * @param type 返回的对象类型
+     * @param sql  执行的sql语句
+     * @param args sql对应的参数值
+     * @param <T>  返回的类型的泛型
+     * @return
+     */
+    public <T> Object queryForOne(Class<T> type, String sql, Object... args) {
+        //連結對象
+        Connection connection = JdbcUtils.getConnection();
+        try {
+            return queryRunner.query(connection, sql, new BeanHandler<T>(type), args);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            JdbcUtils.close(connection);
+        }
+        return null;
+    }
+
+    /**
+     * 查询返回多个javaBean的sql语句
+     *
+     * @param type 返回的对象类型
+     * @param sql  执行的sql语句
+     * @param args sql对应的参数值
+     * @param <T>  返回的类型的泛型
+     * @return
+     */
+
+    public <T> Object queryForList(Class<T> type, String sql, Object... args) {
+        //連結對象
+        Connection connection = JdbcUtils.getConnection();
+        try {
+            return queryRunner.query(connection, sql, new BeanListHandler<T>(type), args);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            JdbcUtils.close(connection);
+        }
+        return null;
+    }
+
+    /**
+     * 执行返回一行一列的sql语句
+     * @param sql   执行的sql语句
+     * @param args  sql对应的参数值
+     * @return
+     */
+    public Object queryForSingleValue(String sql, Object... args){
+
+        Connection connection = JdbcUtils.getConnection();
+
+        try {
+            return queryRunner.query(connection, sql, new ScalarHandler(), args);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            JdbcUtils.close(connection);
+        }
+        return null;
+
+    }
+
+}
